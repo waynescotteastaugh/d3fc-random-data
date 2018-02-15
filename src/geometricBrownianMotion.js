7cb783cbc1607cdbb70acdb604f1e45103f79f87
Wayne Eastaugh <weastaugh@scottlogic.co.uk>
2018-02-15 16:34:32 +0000

    fix: add default seed value of 0

--- src/geometricBrownianMotion.js
+++ src/geometricBrownianMotion.js
@@ -1,20 +1,21 @@
 import { randomNormal as _randomNormal } from 'd3-random';
 
-export default function() {
+/* eslint space-before-function-paren: "off" */
+export default function () {
     let period = 1;
     let steps = 20;
     let mu = 0.1;
     let sigma = 0.1;
     let randomNormal = _randomNormal();
 
-    var geometricBrownianMotion = value => {
+    var geometricBrownianMotion = (value = 0) => {
         const timeStep = period / steps;
         const pathData = [];
 
         for (let i = 0; i < steps + 1; i++) {
             pathData.push(value);
             const increment = (randomNormal() * Math.sqrt(timeStep) * sigma) +
-                 ((mu - sigma * sigma / 2) * timeStep);
+                ((mu - sigma * sigma / 2) * timeStep);
             value = value * Math.exp(increment);
         }
 
